aee947767032f7c6b9a610ce8551336b6a16a0a0
Sean Kerr <sean@code-box.org>
2015-09-23 12:16:54 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+// +-----------------------------------------------------------------------------------------------+
+// | Copyright 2015 Sean Kerr                                                                      |
+// |                                                                                               |
+// | Licensed under the Apache License, Version 2.0 (the "License");                               |
+// | you may not use this file except in compliance with the License.                              |
+// | You may obtain a copy of the License Author                                                   |
+// |                                                                                               |
+// |  http://www.apache.org/licenses/LICENSE-2.0                                                   |
+// |                                                                                               |
+// | Unless required by applicable law or agreed to in writing, software                           |
+// | distributed under the License is distributed on an "AS IS" BASIS,                             |
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                      |
+// | See the License for the specific language governing permissions and                           |
+// | limitations under the License.                                                                |
+// +-----------------------------------------------------------------------------------------------+
+// | Author: Sean Kerr <sean@code-box.org>                                                         |
+// +-----------------------------------------------------------------------------------------------+
+
+#![allow(non_camel_case_types)]
+
+extern crate libc;
+
+use libc::{ c_char,  c_uchar,
+            int16_t, uint16_t,
+            int32_t, uint32_t,
+            int64_t, uint64_t };
+
+// -------------------------------------------------------------------------------------------------
+// TYPES
+// -------------------------------------------------------------------------------------------------
+
+pub type khronos_float_t  = f32;
+pub type khronos_int8_t   = c_char;
+pub type khronos_uint8_t  = c_uchar;
+pub type khronos_int16_t  = int16_t;
+pub type khronos_uint16_t = uint16_t;
+pub type khronos_int32_t  = int32_t;
+pub type khronos_uint32_t = uint32_t;
+pub type khronos_int64_t  = int64_t;
+pub type khronos_uint64_t = uint64_t;
+
+pub type khronos_intptr_t  = int32_t;
+pub type khronos_uintptr_t = uint32_t;
+pub type khronos_ssize_t   = int32_t;
+pub type khronos_usize_t   = uint32_t;
+
+pub type khronos_stime_nanoseconds_t = khronos_int64_t;
+pub type khronos_utime_nanoseconds_t = khronos_uint64_t;
