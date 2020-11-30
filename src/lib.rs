24f14b53a1accc5c60732b8062ffccef659504c7
Dzmitry Malyshau <kvarkus@gmail.com>
2020-11-30 16:47:37 -0500

    Remove libc dependency and bump the version

--- src/lib.rs
+++ src/lib.rs
@@ -16,39 +16,26 @@
 // | Author: Sean Kerr <sean@metatomic.io>                                                         |
 // +-----------------------------------------------------------------------------------------------+
 
-#![crate_name = "khronos"]
 #![allow(non_camel_case_types)]
-
-extern crate libc;
-
-use libc::{ c_float,
-            int8_t,
-            int16_t,
-            int32_t,
-            int64_t,
-            uint8_t,
-            uint16_t,
-            uint32_t,
-            uint64_t };
 
 // -------------------------------------------------------------------------------------------------
 // TYPES
 // -------------------------------------------------------------------------------------------------
 
-pub type khronos_float_t  = c_float;
-pub type khronos_int8_t   = int8_t;
-pub type khronos_uint8_t  = uint8_t;
-pub type khronos_int16_t  = int16_t;
-pub type khronos_uint16_t = uint16_t;
-pub type khronos_int32_t  = int32_t;
-pub type khronos_uint32_t = uint32_t;
-pub type khronos_int64_t  = int64_t;
-pub type khronos_uint64_t = uint64_t;
+pub type khronos_float_t = f32;
+pub type khronos_int8_t = i8;
+pub type khronos_uint8_t = u8;
+pub type khronos_int16_t = i16;
+pub type khronos_uint16_t = u16;
+pub type khronos_int32_t = i32;
+pub type khronos_uint32_t = u32;
+pub type khronos_int64_t = i64;
+pub type khronos_uint64_t = u64;
 
-pub type khronos_intptr_t  = int32_t;
-pub type khronos_uintptr_t = uint32_t;
-pub type khronos_ssize_t   = int32_t;
-pub type khronos_usize_t   = uint32_t;
+pub type khronos_intptr_t = i32;
+pub type khronos_uintptr_t = u32;
+pub type khronos_ssize_t = i32;
+pub type khronos_usize_t = u32;
 
-pub type khronos_stime_nanoseconds_t = khronos_int64_t;
-pub type khronos_utime_nanoseconds_t = khronos_uint64_t;
+pub type khronos_stime_nanoseconds_t = i64;
+pub type khronos_utime_nanoseconds_t = u64;
